92d0a9ccb5d6eec2078a305e58379f1550bad0c6
Hetsh <git@hetsh.de>
2021-03-03 13:28:15 +0100

    Remove ignored hashrate report

--- windows/cuda/NiceHash.bat
+++ windows/cuda/NiceHash.bat
@@ -6,7 +6,6 @@ SET NAME=GITHUB
 :START
 "C:\Program Files\ETHMiner\ethminer.exe"^
 	--cuda^
-	--report-hr^
 	--pool stratum://%ADDRESS%.%NAME%@daggerhashimoto.eu-north.nicehash.com:3353^
 	--pool stratum://%ADDRESS%.%NAME%@daggerhashimoto.eu-west.nicehash.com:3353
 IF ERRORLEVEL 1 GOTO START
