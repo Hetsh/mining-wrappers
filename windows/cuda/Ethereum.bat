a6d60a2f1a1f768f12ced49884e66e06e841fad9
Hetsh <git@hetsh.de>
2021-03-03 13:24:41 +0100

    Restart miner after crash

--- windows/cuda/Ethereum.bat
+++ windows/cuda/Ethereum.bat
@@ -4,8 +4,10 @@ SET ADDRESS=0x0A40808201aE52D1869c82895353364Ab16ba200
 SET NAME=GITHUB
 SET PASSWORD=CarefulWithSpecialChars
 
+:START
 "C:\Program Files\ETHMiner\ethminer.exe"^
 	--cuda^
 	--report-hr^
 	--pool stratum://%ADDRESS%@eth-eu2.nanopool.org:9999/%NAME%/%PASSWORD%^
 	--pool stratum://%ADDRESS%@eth-eu1.nanopool.org:9999/%NAME%/%PASSWORD%
+IF ERRORLEVEL 1 GOTO START
--- windows/cuda/Raven.bat
+++ windows/cuda/Raven.bat
@@ -4,8 +4,10 @@ SET ADDRESS=RBmRtxhbVwF9wqho11G8jCNY7M22NwJoEL
 SET NAME=GITHUB
 SET PASSWORD=CarefulWithSpecialChars
 
+:START
 "C:\Program Files\KawPowMiner\kawpowminer.exe"^
 	--cuda^
 	--report-hr^
 	--pool stratum+tcp://%ADDRESS%.%NAME%/%PASSWORD%@rvn-eu2.nanopool.org:12222^
 	--pool stratum+tcp://%ADDRESS%.%NAME%/%PASSWORD%@rvn-eu1.nanopool.org:12222
+IF ERRORLEVEL 1 GOTO START
